0c0d51a10664e9fdc39e3b778fa498af31745276
Aicharjya Baruah <aicharjya.baruah2002@gmail.com>
2026-08-23 01:45:11 +0530

    Add backend status route

--- server/src/app.js
+++ server/src/app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Library Management System Backend Is Running...");
+});
+
 module.exports = app;
